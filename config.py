3de408de4d2c884b4abcf1566a2f6aaaecd0d520
AdonaiFP <donanosiaxd@gmail.com>
2026-02-12 20:59:29 -0600

    12/02/2026 Archivo de configuracion

---
+++ config.py
@@ -0,0 +1,10 @@
+from sqlalchemy import create_engine
+
+class Config(object):
+    SECRET_KET = "ClaveSecreta"
+    SESSION_COOKIE_SECURE = False
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'msql+pymsql://root:root@127.0.0.1/bdidgs803'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
